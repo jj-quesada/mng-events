b763c342bbf4a95b62611914e4422cd9647ec2a6
Álvaro <alvarosacosta22@gmail.com>
2025-03-21 17:33:53 +0000

    feature: added sidebar

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import vuetify from './vuetify';
 import { createPinia } from "pinia";
+import { createVuetify } from "vuetify";
+import 'vuetify/styles'
 
+const vuetify = createVuetify()
 const pinia = createPinia();
+
 createApp(App).use(router).use(vuetify).use(pinia).mount("#app");
